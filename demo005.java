8a78e5dcbcc4d69881fa6549d3692c94b2243581
Udaykiran599 <salluriudaykiran96@gmail.com>
2025-01-04 10:39:57 +0530

    Add files via upload

---
+++ demo005.java
@@ -0,0 +1,11 @@
+
+public class demo005{
+    public static void main(String[] args) {
+        String username = "user001";
+        String pwd = "user_001";
+        while((username == "user001") && (pwd == "user_001")) {
+        System.err.println("hello happy new year");
+            break;
+        }
+    } 
+}
