42b51c7145f9693589a152487f7dd25771d99183
Udaykiran599 <salluriudaykiran96@gmail.com>
2025-01-22 10:20:09 +0530

    Add files via upload

---
+++ LT001.java
@@ -0,0 +1,22 @@
+class Solution {
+ 
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));  // Output: true
+        System.out.println(isPalindrome(-121)); // Output: false
+        System.out.println(isPalindrome(10));   // Output: false
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int original = x;
+        int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+}
